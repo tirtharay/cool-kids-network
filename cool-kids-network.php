b9dee8002b10e250dce5f5692ed65b53f1cdf44d
tirtharay <contato@tirtharay.com.br>
2024-10-11 19:33:59 +0530

    default plugin

---
+++ cool-kids-network.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Cool Kids Network
+ * Description: A plugin for managing custom user roles and login functionality for the Cool Kids Network.
+ * Version: 1.0
+ * Author: Your Name
+ * Text Domain: cool-kids-network
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
