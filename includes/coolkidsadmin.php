6133a622682f4b1d55c405b9cc9b55deada6ee89
tirtharay <contato@tirtharay.com.br>
2024-10-24 00:31:43 +0530

    fix

--- includes/coolkidsadmin.php
+++ includes/coolkidsadmin.php
@@ -1,18 +1,16 @@
 <?php
-/**
- * Cool Kids Admin class
- * This class handles the creation of the admin menu for the Cool Kids Network plugin.
- */
 class CoolKidsAdmin {
 
-    //Import classes
+    private $user_list;
     private $demo_users_generator;
 
     public function __construct() {
+        add_action('admin_menu', [$this, 'add_admin_menu']);
 
-        add_action('admin_menu', [$this, 'add_admin_menu']);
-        // Initialize the demo users generator
-        add_action('admin_menu', [$this, 'add_admin_menu']);
+        // Initialize the user list class (autoload will handle loading it)
+        $this->user_list = new CoolKidsUserList();
+
+        // Initialize the demo users generator class
         $this->demo_users_generator = new CoolKidsDemoUsersGenerator();
     }
 
@@ -26,7 +24,7 @@ class CoolKidsAdmin {
             'Cool Kids Network',  // Menu title
             'manage_options',     // Capability required
             'cool-kids-network',  // Menu slug
-            [$this, 'display_settings_page'],  // Function to display the page content
+            [$this, 'display_user_list'],  // Function to display the user list
             'dashicons-groups',   // Icon
             6                     // Position
         );
@@ -38,28 +36,25 @@ class CoolKidsAdmin {
             'Settings',           // Menu title
             'manage_options',     // Capability required
             'cool-kids-network-settings', // Menu slug
-            [$this, 'display_settings_page'] // Function to display the page content
+            [$this, 'display_settings_page'] // Function to display the settings page (Demo users generation form)
         );
     }
 
     /**
-     * Display the content for the Settings page.
+     * Display the user list by calling the CoolKidsUserList class.
+     */
+    public function display_user_list() {
+        $this->user_list->display_user_list(); // Call the method to display the user list
+    }
+
+    /**
+     * Display the content for the Settings page (Generate Demo Users form).
      */
     public function display_settings_page() {
         ?>
         <div class="wrap">
             <h1><?php esc_html_e('Cool Kids Network Settings', 'cool-kids-network'); ?></h1>
-
-            <?php if (isset($_GET['demo_users_generated'])): ?>
-                <div class="notice notice-success">
-                    <p><?php printf(esc_html__('%d demo users were successfully created.', 'cool-kids-network'), intval($_GET['demo_users_generated'])); ?></p>
-                </div>
-            <?php endif; ?>
-
-            <?php
-            // Display the demo users generator form
-            $this->demo_users_generator->display_form();
-            ?>
+            <?php $this->demo_users_generator->display_form(); // Call the method to display the Generate Demo Users form ?>
         </div>
         <?php
     }
